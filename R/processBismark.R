1665e969b1ca09992af34b1859e43d381ee5da29
Marcin <bazyliszek@users.noreply.github.com>
2020-02-21 19:12:25 +0100

    Update processBismark.R

--- R/processBismark.R
+++ R/processBismark.R
@@ -17,11 +17,11 @@
 
 
 myworkingdir <- '/WORKING/projects/DNAm_testis/Sverige01/bismark_methylation_calls/downstream/Transformed'
-print(myworkingdir)
+setwd(myworkingdir)
 
 #path = getwd()
 
-processBismark <- function(files = list.files(path = setwd(myworkingdir), pattern = "*.txt.gz"),
+processBismark <- function(files = list.files(pattern = "*.txt.gz"),
                            meta = openxlsx::read.xlsx("sample_info.xlsx", colNames = TRUE) %>% dplyr::mutate_if(is.character, as.factor),
                            testCovar = testCovariate,
                            adjustCovar = NULL,
